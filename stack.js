778c4b8a10be0eaa86d1f3765e28040f7c8d0ced
Ray Agni <jagni001@ucr.edu>
2021-08-30 15:36:18 -0700

    FEAT: finished all methods for the queues and stacks

--- stack.js
+++ stack.js
@@ -19,26 +19,40 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    if (this.top === null) {
+      this.top = newNode;
+      this.size++;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+      this.size++;
+    }
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.top === null) throw new Error("The stack is empty.");
+    else {
+      let val = this.top.val;
+      this.top = this.top.next;
+      this.size--;
+      return val;
+    }
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
